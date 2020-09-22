98516f786a944234690f1f417c8886a1fe070467
Vyacheslav <honeydrums@icloud.com>
2020-09-22 16:34:08 +0300

    beautify css, make js and add icons

---
+++ js/script.js
@@ -0,0 +1,9 @@
+document.querySelector('.top-header__btns_search-label').addEventListener('click', function() {
+    document.querySelector('#searchInput').classList.toggle('display-none');
+    document.querySelector('.top-header').classList.toggle('top-header__mobile');
+    document.querySelector('.top-header__search-form').classList.toggle('search-mobile');
+});
+
+document.querySelector('.hamburger--squeeze').addEventListener('click', function() {
+    this.classList.toggle('is-active');
+});
