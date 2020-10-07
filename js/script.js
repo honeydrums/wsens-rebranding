a3008a1a1ed3a974eb9fb645e6049ac6173c89c3
Vyacheslav <honeydrums@icloud.com>
2020-10-07 17:53:04 +0300

    верстаю попап

--- js/script.js
+++ js/script.js
@@ -13,3 +13,20 @@ document.querySelector('.hamburger').addEventListener('click', function() {
     this.classList.toggle('is-active');
     document.querySelector('.bottom-header__nav-list').classList.toggle('bottom-header__nav-list__mobile')
 });
+
+// отключим кнопку отправить/зарегистрироваться, если не чекнут чекбокс с соглашением
+
+let checkBox = document.querySelector('.checkbox-wrap input[name="confirm-yes"]');
+let submitBtn = document.querySelector('input[type="submit"]');
+
+function checkPropButton() {
+    if (checkBox.checked === false) {
+        submitBtn.disabled = true;
+        submitBtn.classList.add('too-many-symbols__button');
+        console.log('nope');
+    } else {
+        submitBtn.disabled = false;
+        submitBtn.classList.remove('too-many-symbols__button');
+        console.log('yep');
+    }
+};
