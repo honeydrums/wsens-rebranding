6f2347ac43ecad8bd20b9e5fd8ff67c0e661b205
Vyacheslav <46050023+honeydrums@users.noreply.github.com>
2020-10-09 00:11:29 +0300

    minor

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
-let searchIcon = document.querySelector('.top-header__btns_search-label');
-let searchInput = document.querySelector('#searchInput');
-let topHeader = document.querySelector('.top-header');
-let searchForm = document.querySelector('.top-header__search-form');
+// функция показа поля поиска
+function showSearchField() {
+    let searchIcon = document.querySelector('.top-header__btns_search-label');
+    let searchInput = document.querySelector('#searchInput');
+    let topHeader = document.querySelector('.top-header');
+    let searchForm = document.querySelector('.top-header__search-form');
 
-searchIcon.addEventListener('click', function() {
     searchInput.classList.toggle('display-none');
     topHeader.classList.toggle('top-header__mobile');
     searchForm.classList.toggle('search-mobile');
-});
+};
 
 document.querySelector('.hamburger').addEventListener('click', function() {
     this.classList.toggle('is-active');
-    document.querySelector('.bottom-header__nav-list').classList.toggle('bottom-header__nav-list__mobile')
 });
 
 // отключим кнопку отправить/зарегистрироваться, если не чекнут чекбокс с соглашением
